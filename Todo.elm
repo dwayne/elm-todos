91b8beb6d3134c1dc2981956e9306f6ed7c53543
Dwayne Crooks <me@dwaynecrooks.com>
2018-06-15 11:17:30 -0400

    Keep track of the description

--- Todo.elm
+++ Todo.elm
@@ -2,11 +2,45 @@ module Todo exposing (main)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events as Events
 
+main : Program Never Model Msg
 main =
+  Html.beginnerProgram
+    { model = model
+    , update = update
+    , view = view
+    }
+
+-- MODEL
+
+type alias Model =
+  { description : String }
+
+model : Model
+model =
+  { description = "" }
+
+-- UPDATE
+
+type Msg
+  = SetDescription String
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    SetDescription description ->
+      { model | description = description }
+
+-- VIEW
+
+view : Model -> Html Msg
+view { description } =
   input
     [ type_ "text"
     , autofocus True
     , placeholder "What needs to be done?"
+    , value description
+    , Events.onInput SetDescription
     ]
     []
