ce49aee93036323e7933f8e44e1cb26c162a7924
Dwayne Crooks <me@dwaynecrooks.com>
2025-10-21 08:48:35 -0400

    Add support for compression

--- elm2nix/build.nix
+++ elm2nix/build.nix
@@ -1,16 +1,9 @@
 #
 # N.B. I can bring everything together into mkElmDerivation.
 #
-# What about elm-optimize-level-2?
-# What about terser instead of uglify-js?
-# What about compression?
-#
-# What about using multiple outputs: out, doc
-#
-# Make use of install where appropriate
-#
 
-{ elmPackages
+{ brotli
+, elmPackages
 , lib
 , stdenv
 , uglify-js
@@ -24,6 +17,7 @@
 , debug ? false
 , optimize ? false
 , minify ? false
+, compress ? false
 }:
 let
   outputMin = "${lib.removeSuffix ".js" output}.min.js";
@@ -34,7 +28,8 @@ stdenv.mkDerivation {
   nativeBuildInputs = [
     elmPackages.elm
   ]
-  ++ lib.optional minify uglify-js;
+  ++ lib.optional minify uglify-js
+  ++ lib.optional compress brotli;
 
   buildPhase = ''
     ${dotElm.prepareScript}
@@ -56,6 +51,11 @@ stdenv.mkDerivation {
       uglifyjs ".build/${output}" \
         --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
         | uglifyjs --mangle --output ".build/${outputMin}"
+    ''}
+
+    ${lib.optionalString compress ''
+      gzip -9 -c ".build/${outputMin}" > ".build/${outputMin}.gz"
+      brotli -Z -c ".build/${outputMin}" > ".build/${outputMin}.br"
     ''}
   '';
 
--- flake.nix
+++ flake.nix
@@ -6,22 +6,6 @@
       lib = pkgs.lib;
 
       #
-      # 2. I want to be able to name the JavaScript file that's output.
-      #
-
-      #
-      # 3. What if I want to use a different Elm compiler?
-      #
-      # For example: elm-optimize-level-2
-      #
-
-      #
-      # 4. What if I want to use a different minifier?
-      #
-      # For example: terser
-      #
-
-      #
       # 5. It would be nice to explore how I can expose a NixOS configuration to host my Elm web application.
       #
 
@@ -81,6 +65,7 @@
           src = ./.;
           inherit dotElm;
           minify = true;
+          compress = true;
         };
       };
 
