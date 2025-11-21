6b43436534ec07ad9719fb587324d705e2463f87
Dwayne Crooks <me@dwaynecrooks.com>
2025-11-21 07:06:09 -0400

    Add a flake.nix with a developer shell that has dwayne/elm2nix installed

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    elm2nix = {
+      url = "git+ssh://git@github.com/dwayne/elm2nix?rev=4fa3a3c882eebbe649c6edfbde7b11ba017a6cc1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, elm2nix }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      lib = pkgs.lib;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "dev";
+
+        packages = [
+          elm2nix.packages.${system}.default
+        ];
+
+        shellHook = ''
+          export PS1="($name) $PS1"
+        '';
+      };
+    };
+}
