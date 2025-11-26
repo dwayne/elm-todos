27adeead67173cc3e70e67169e7109430500200f
Dwayne Crooks <me@dwaynecrooks.com>
2025-11-26 06:15:59 -0400

    Use flake-utils

--- flake.nix
+++ flake.nix
@@ -3,49 +3,51 @@
     elm2nix = {
       url = "git+ssh://git@github.com/dwayne/elm2nix?rev=2216e3efe9145142acb837078257ee74c576c20d";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
-  outputs = { self, nixpkgs, elm2nix }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      lib = pkgs.lib;
+  outputs = { self, nixpkgs, flake-utils, elm2nix }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib;
 
-      e2n = pkgs.callPackage ./elm2nix.nix {};
+        e2n = pkgs.callPackage ./elm2nix.nix {};
 
-      elmTodos = e2n.mkElmDerivation {
-        name = "elm-todos";
-        src = ./.;
-        elmLock = ./elm.lock;
-        registryDat = ./registry.dat;
-        output = "app.js";
-        enableOptimizations = true;
-        enableMinification = true;
-        useTerser = true;
-        enableCompression = true;
-      };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        name = "dev";
+        elmTodos = e2n.mkElmDerivation {
+          name = "elm-todos";
+          src = ./.;
+          elmLock = ./elm.lock;
+          registryDat = ./registry.dat;
+          output = "app.js";
+          enableOptimizations = true;
+          enableMinification = true;
+          useTerser = true;
+          enableCompression = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "dev";
 
-        packages = [
-          elm2nix.packages.${system}.default
-        ];
+          packages = [
+            elm2nix.packages.${system}.default
+          ];
 
-        shellHook = ''
-          export PS1="($name) $PS1"
-        '';
-      };
+          shellHook = ''
+            export PS1="($name) $PS1"
+          '';
+        };
 
-      packages.${system} = {
-        inherit elmTodos;
-        default = elmTodos;
-      };
+        packages = {
+          inherit elmTodos;
+          default = elmTodos;
+        };
 
-      lib = {
-        inherit (e2n) preConfigure dotElmLinks symbolicLinksToPackages fetchElmPackage;
-      };
-    };
+        lib = {
+          inherit (e2n) preConfigure dotElmLinks symbolicLinksToPackages fetchElmPackage;
+        };
+      }
+    );
 }
