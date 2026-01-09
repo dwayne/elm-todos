7e9a08fcda2e14cce28f69dcd5f845de9d65e4ff
Dwayne Crooks <me@dwaynecrooks.com>
2026-01-09 16:42:24 -0400

    Build the Elm web application with elm2nix

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    elm2nix = {
+      url = "github:dwayne/elm2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, elm2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        inherit (elm2nix.lib.elm2nix pkgs) buildElmApplication;
+
+        elmTodos = buildElmApplication {
+          name = "elm-todos";
+          src = ./.;
+          elmLock = ./elm.lock;
+          output = "app.js";
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "elm-todos";
+
+          packages = [
+            elm2nix.packages.${system}.default
+          ];
+
+          shellHook = ''
+            export PS1="($name)\n$PS1"
+          '';
+        };
+
+        packages = {
+          inherit elmTodos;
+          default = elmTodos;
+        };
+      }
+    );
+}
