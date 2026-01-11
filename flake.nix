a0ce5d607234ecc7cdfab80ca99647ceeefba37b
Dwayne Crooks <me@dwaynecrooks.com>
2026-01-11 05:36:23 -0400

    Add support for serving the application using Caddy

--- flake.nix
+++ flake.nix
@@ -30,6 +30,14 @@
             outputMin = "app.js";
           };
         };
+
+        appDev = pkgs.writeShellScript "app-dev" ''
+          port=8000 root=${dev} ${pkgs.caddy}/bin/caddy run --config ${./Caddyfile} --adapter caddyfile
+        '';
+
+        appProd = pkgs.writeShellScript "app-dev" ''
+          port=8000 root=${prod} ${pkgs.caddy}/bin/caddy run --config ${./Caddyfile} --adapter caddyfile
+        '';
       in
       {
         devShells.default = pkgs.mkShell {
@@ -37,6 +45,7 @@
 
           packages = [
             elm2nix.packages.${system}.default
+            pkgs.caddy # Added so that I could format the Caddyfile, i.e. caddy fmt --overwrite Caddyfile
           ];
 
           shellHook = ''
@@ -47,6 +56,20 @@
         packages = {
           inherit dev prod;
           default = dev;
+        };
+
+        apps = {
+          default = self.apps.${system}.appDev;
+
+          appDev = {
+            type = "app";
+            program = "${appDev}";
+          };
+
+          appProd = {
+            type = "app";
+            program = "${appProd}";
+          };
         };
       }
     );
